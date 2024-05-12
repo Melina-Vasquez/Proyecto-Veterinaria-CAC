f38d865e3ff7b7fe2c0e6f0c9da8ac65f56f1914
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2024-05-12 03:03:24 -0300

    formulario listo

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ function validateForm() {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     var message = document.getElementById("message").value;
-    var department = document.getElementById("department").value;
+    var plan = document.getElementById("plan").value;
     var gender = document.querySelector('input[name="gender"]:checked');
     var image = document.getElementById("image").value;
     var isValid = true;
 
-    // Validación de nombre, email, teléfono, mensaje, departamento, intereses, género y archivo de imagen
 
-    // Validación de nombre
+
+
     if (name.trim() === "") {
         document.getElementById("nameError").innerText = "Por favor, ingrese su nombre.";
         isValid = false;
@@ -18,7 +18,7 @@ function validateForm() {
         document.getElementById("nameError").innerText = "";
     }
 
-    // Validación de email
+
     if (email.trim() === "") {
         document.getElementById("emailError").innerText = "Por favor, ingrese su email.";
         isValid = false;
@@ -29,7 +29,7 @@ function validateForm() {
         document.getElementById("emailError").innerText = "";
     }
 
-    // Validación de teléfono
+
     if (phone.trim() === "") {
         document.getElementById("phoneError").innerText = "Por favor, ingrese su número de teléfono.";
         isValid = false;
@@ -37,7 +37,7 @@ function validateForm() {
         document.getElementById("phoneError").innerText = "";
     }
 
-    // Validación de mensaje
+
     if (message.trim() === "") {
         document.getElementById("messageError").innerText = "Por favor, ingrese su mensaje.";
         isValid = false;
@@ -45,16 +45,16 @@ function validateForm() {
         document.getElementById("messageError").innerText = "";
     }
 
-    // Validación de departamento
-    if (department === "") {
-        document.getElementById("departmentError").innerText = "Por favor, seleccione un departamento.";
+
+    if (plan === "") {
+        document.getElementById("planError").innerText = "Por favor, seleccione un departamento.";
         isValid = false;
     } else {
-        document.getElementById("departmentError").innerText = "";
+        document.getElementById("planError").innerText = "";
     }
 
 
-    // Validación de género (al menos uno seleccionado)
+
     if (!gender) {
         document.getElementById("genderError").innerText = "Por favor, seleccione su género.";
         isValid = false;
@@ -62,7 +62,7 @@ function validateForm() {
         document.getElementById("genderError").innerText = "";
     }
 
-    // Validación de imagen (opcional: verificar si se ha seleccionado un archivo)
+
     if (image === "") {
         document.getElementById("imageError").innerText = "Por favor, seleccione una imagen.";
         isValid = false;
@@ -70,23 +70,21 @@ function validateForm() {
         document.getElementById("imageError").innerText = "";
     }
 
-    // Si el formulario es válido, continuar con el envío
+ 
     if (isValid) {
-        // Mostrar el modal y redireccionar
         var modal = document.getElementById("myModal");
         modal.style.display = "block";
         setTimeout(function() {
             modal.style.display = "none";
             window.location.reload();
-        }, 5000);
+        }, 3000);
     }
 
-    // Detener el envío del formulario
+
     return false;
 }
 
 function isValidEmail(email) {
-    // Expresión regular para validar email
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
 }
